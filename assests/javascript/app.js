5f972a8e7f9b6a2217e357654bc13c6c10a61cfb
njorogepeter <pgakonde@yahoo.com>
2019-01-20 08:09:59 -0800

    questions and id tages

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -4,3 +4,47 @@
 // check clicks against answer
 // display correct score
 // display incorrect score
+
+var worldCupTrivia = [
+    {
+        question: "Which nation has not hosted the world cup within the last ten years?",
+        answers: ["Brazil", "South Africa", "England", "Russia"],
+        correct: "England"
+    },
+
+    {
+        question: "Country with the most world cup wins?",
+        answers: ["Argentina", "France", "Germany", "Brazil"],
+        correct: "Brazil"
+    },
+
+    {
+        question: "First world cup game was played in?",
+        answers: ["Europe", "South America", "Asia", "North America"],
+        correct: "South America"
+    },
+
+    {
+        question: "Country to win only title as a host country?",
+        answers: ["England", "Uruguay", "Italy", "France"],
+        correct: "England"
+    },
+   
+    {
+        question: "Pele played for which country?",
+        answers: ["Mexico", "Argentina", "Brazil", "Uruguay"],
+        correct: "Brazil"
+    },
+
+    {
+        question: "The most goals scored by a player is?",
+        answers: ["21","32","10","16"],
+        correct: "16"
+    },
+
+    {
+        question: "Teams that participate in the tournament?",
+        answers: ["64","32","24","16"],
+        correct: "32"
+    }
+]
